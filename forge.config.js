cb3c5c2fce2b91fab9de15fba863463bd241a226
bm-avatar <bmatusiak@gmail.com>
2024-02-09 09:21:40 -0500

    drafting

--- forge.config.js
+++ forge.config.js
@@ -11,8 +11,7 @@ module.exports = {
           owner: 'bmatusiak',
           name: 'react-desktop-electron'
         },
-        // draft: true,
-        prerelease: true
+        draft: true
       }
     }
   ],
